06d29c7662dca2f3a166679f90a13b34353850c5
angelina-tsuboi <angelina.t1832@gmail.com>
2023-08-17 09:28:52 -0700

    feat: update web app

--- webapp/app.py
+++ webapp/app.py
@@ -1,31 +1,23 @@
-from flask import Flask,request, url_for, redirect, render_template
-import pickle
 import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-
-model=pickle.load(open('model.pkl','rb'))
-
+model = pickle.load(open('model.pkl', 'rb'))# loads ML model
 
 @app.route('/')
-def hello_world():
-    return render_template("forest_fire.html")
+def home():
+    return render_template('index.html')# renders index.html
 
-
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict',methods=['POST'])# gets the values that were sent to '/predict' by 'index.html'
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    int_features = [float(x) for x in request.form.values()]# defines the form values in an array
+    final_features = [np.array(int_features)]# turns the form values into a Numpy array
+    prediction = model.predict(final_features)# makes a prediction using the values in the created Numpy array
 
-    if output>str(0.5):
-        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    else:
-        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+    output = prediction[0]# gets the prediction as a string
 
+    return render_template('index.html', prediction_text='Iris species: {}'.format(output))# displays the prediction inside the '<b>{{ prediction_text }}</b>' that we've seen in 'index.html'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)# Runs the Web App
