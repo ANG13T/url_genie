b8eccfe7382170677caf6e2187fadc7927173c90
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-08-19 12:21:13 -0700

    feat: update notebooks

--- webapp/app.py
+++ webapp/app.py
@@ -1,1 +1,25 @@
 import streamlit as st
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization ,Activation
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+model = Sequential()
+model.add(Dense(32, activation = 'relu', input_shape = (16, )))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu')) 
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+
+opt = keras.optimizers.Adam(lr=0.0001)
+model.compile(optimizer= opt ,loss='binary_crossentropy',metrics=['acc'])
+
+history = model.fit(x_train, y_train, epochs=10,batch_size=256, callbacks=[callback],validation_data=(x_test,y_test),verbose=1)
+
