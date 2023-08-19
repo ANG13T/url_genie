e7bf6276f3346cdd3150aff173dd3dc01dfe76b3
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-08-19 12:25:28 -0700

    feat: init app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow import keras
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.callbacks import ModelCheckpoint
@@ -9,6 +13,39 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization ,Activation
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import ModelCheckpoint
+
+urldata = pd.read_csv("./Url_Processed.csv")
+
+# droping "Unnamed: 0" as its unncessary feature
+urldata.drop("Unnamed: 0",axis=1,inplace=True)
+
+# remove uneccessary columns
+urldata.drop(["url","label"],axis=1,inplace=True)
+
+urldata.head()
+
+x = urldata[['hostname_length',
+       'path_length', 'fd_length', 'count-', 'count@', 'count?',
+       'count%', 'count.', 'count=', 'count-http','count-https', 'count-www', 'count-digits',
+       'count-letters', 'count_dir', 'use_of_ip']]
+
+#Dependent Variable
+y = urldata['result']
+
+x_sample, y_sample = SMOTE().fit_resample(x, y.values.ravel())
+
+x_sample = pd.DataFrame(x_sample)
+y_sample = pd.DataFrame(y_sample)
+
+# checking the sizes of the sample data
+print("Size of x-sample :", x_sample.shape)
+print("Size of y-sample :", y_sample.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x_sample, y_sample, test_size = 0.2)
+print("Shape of x_train: ", x_train.shape)
+print("Shape of x_valid: ", x_test.shape)
+print("Shape of y_train: ", y_train.shape)
+print("Shape of y_valid: ", y_test.shape)
 
 model = Sequential()
 model.add(Dense(32, activation = 'relu', input_shape = (16, )))
@@ -23,3 +60,25 @@ model.compile(optimizer= opt ,loss='binary_crossentropy',metrics=['acc'])
 
 history = model.fit(x_train, y_train, epochs=10,batch_size=256, callbacks=[callback],validation_data=(x_test,y_test),verbose=1)
 
+# list all data in history
+print(history.history.keys())
+
+# summarize history for accuracy
+plt.figure(figsize=(20,8))
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.figure(figsize=(20,8))
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
