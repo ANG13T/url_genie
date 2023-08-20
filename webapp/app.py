c3445e877f2ba3b8aed5820434394bea295b3eb9
angelina-tsuboi <angelina.t1832@gmail.com>
2023-08-19 17:07:15 -0700

    feat: update app structure

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-image = "/assets/url_genie_logo.png"
+image = "https://github.com/ANG13T/url_genie/blob/main/webapp/assets/url_genie_logo.png?raw=true"
 
 col1, col2, col3 = st.columns(3)
 
@@ -13,5 +13,8 @@ with col2:
 
 with col3:
     st.write(' ')
-st.title("Malicious Domain Name Detection Model made using Python")
+
+st.markdown("<h1 style='text-align: center; color: #14559E'>URL Genie</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: #494848;'>Malicious URL Detection Model made using Python</h3>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #494848;'>Malicious URL Detection Model made using Python</p>", unsafe_allow_html=True)
 st.text_input("Enter URL to scan", "https://google.com")
