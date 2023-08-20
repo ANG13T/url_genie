33882c0bce7077a40696e4f7f7ff89fda74040f8
angelina-tsuboi <angelina.t1832@gmail.com>
2023-08-19 17:36:34 -0700

    feat: update app

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,13 @@
 import streamlit as st
-from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
 
-image = "https://github.com/ANG13T/url_genie/blob/main/webapp/assets/url_genie_logo.png?raw=true"
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model=tf.keras.models.load_model('Malicious_URL_Prediction.h5')
+    return model
+with st.spinner("Loading Model...."):
+    model=load_model()
 
 col1, col2, col3 = st.columns(3)
 
@@ -9,12 +15,21 @@ with col1:
     st.write(' ')
 
 with col2:
-    st.image(image)
+    st.image("https://github.com/ANG13T/url_genie/blob/main/webapp/assets/url_genie_logo.png?raw=true")
 
 with col3:
     st.write(' ')
 
 st.markdown("<h1 style='text-align: center; color: #14559E'>URL Genie</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: #494848;'>Malicious URL Detection Model made using Python</h3>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: #494848;'>Malicious URL Detection Model made using Python</p>", unsafe_allow_html=True)
-st.text_input("Enter URL to scan", "https://google.com")
+st.markdown("<p style='text-align: center; color: #494848;'>This program utilizes a Multilayer Perceptron Neural Network model with optimized hyper-parameters using genetic algorithms to perform malicous URL detection</p>", unsafe_allow_html=True)
+
+def predict(val):
+    st.write("Predicting Class...")
+    with st.spinner("Classifying..."):
+        pred_test = model.predict(val)
+        st.write(pred_test)
+
+
+value = st.text_input("Enter URL to scan", "https://google.com")
+submit = st.button("Classify URL", on_click=predict(value))
